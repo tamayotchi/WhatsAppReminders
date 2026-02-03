286481e8d23aed5fe2b11fe1efb7f6f8f2b09dc6
Juan Tamayo <juancyepest@gmail.com>
2026-02-02 21:28:16 -0500

    remove some credentials

--- src/gateway/dynamoDB/DynamoDBParameters.ts
+++ src/gateway/dynamoDB/DynamoDBParameters.ts
@@ -1,12 +1,19 @@
 import { DynamoDBClientConfig } from "@aws-sdk/client-dynamodb";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const dynamoDBRegion = "us-east-1";
+
 let dynamoDBParameters: DynamoDBClientConfig = {
-  credentials: {
-    accessKeyId: "AKIAUVNPS2IK7QOJOA7Q",
-    secretAccessKey: "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",
-  },
   region: dynamoDBRegion,
 };
+
+if (process.env.AWS_ACCESS_KEY_ID && process.env.AWS_SECRET_ACCESS_KEY) {
+  dynamoDBParameters.credentials = {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  };
+}
 
 export default dynamoDBParameters;
